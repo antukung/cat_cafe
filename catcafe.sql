697eb15df8e2b5a6ade141c7955be095e6ed00c8
Antu <122485403+antukung@users.noreply.github.com>
2023-06-21 20:26:31 -0300

    aca esta algo mas creo el dos y el 3
    Gracias totales por este ejercicio de programacion aprendi mucho profesor

--- catcafe.sql
+++ catcafe.sql
@@ -36,25 +36,49 @@ SELECT * FROM  cat_cafe;
 /* Aca cargo los valores de cada tabla */
 
 INSERT INTO cat_cafe (Nombre, Apodo, Edad, Peso, Gustos, Llego_con_alguien, Color_pelo, Color_ojos)
-VALUES ('Rocket', 'no tiene', 3, 3.0, 'Domir mucho', 'no tiene', 'Blanco', 'Celeste');
+VALUES ('Rocket', '', 3, 3.0, 'Domir mucho', '', 'Blanco', 'Celeste');
 
 INSERT INTO cat_cafe (Nombre, Apodo, Edad, Peso, Gustos, Llego_con_alguien, Color_pelo, Color_ojos)
 VALUES ('Emilia', 'Emili', 5, 4.4, 'Reposar al sol', 'con Sunny' , 'Gris-Negro', 'Amarrilos');
 
 INSERT INTO cat_cafe (Nombre, Apodo, Edad, Peso, Gustos, Llego_con_alguien, Color_pelo, Color_ojos)
-VALUES ('Nikki', 'no tiene', 4, 4.3, 'no se depegaga Ricochet', 'no tiene', 'Gris', 'Verdes-claros');
+VALUES ('Nikki', '', 4, 4.3, 'no se depegaga Ricochet', '', 'Gris', 'Verdes-claros');
 
 INSERT INTO cat_cafe (Nombre, Apodo, Edad, Peso, Gustos, Llego_con_alguien, Color_pelo, Color_ojos)
-VALUES ('Ricochet', 'no tiene', 4, 4.7, 'lugares altos', 'no tiene', 'Blanco-gris', 'Celestes');
+VALUES ('Ricochet', '', 4, 4.7, 'lugares altos', '', 'Blanco-gris', 'Celestes');
 
 INSERT INTO cat_cafe (Nombre, Apodo, Edad, Peso, Gustos, Llego_con_alguien, Color_pelo, Color_ojos)
-VALUES ('Onix', 'no tiene', 7, 3.3, 'Va donde hay comida', 'no tiene', 'Naranaja-Marron', 'Amarillos');
+VALUES ('Onix', '', 7, 3.3, 'Va donde hay comida', '', 'Naranaja-Marron', 'Amarillos');
 
 INSERT INTO cat_cafe (Nombre, Apodo, Edad, Peso, Gustos, Llego_con_alguien, Color_pelo, Color_ojos)
-VALUES ('Seiko', 'no tiene', 2, 3.7, 'Dormir junto a gatos', 'no tiene', 'Blanco-gris', 'Verdosos');
+VALUES ('Seiko', '', 2, 3.7, 'Dormir junto a gatos', '', 'Blanco-gris', 'Verdosos');
 
 INSERT INTO cat_cafe (Nombre, Apodo, Edad, Peso, Gustos, Llego_con_alguien, Color_pelo, Color_ojos)
-VALUES ('Nathan', 'Ninja nate', 7, 4.5, 'se esconde', 'no tiene', 'Negro', 'Amarillentos');
+VALUES ('Nathan', 'Ninja nate', 7, 4.5, 'se esconde', '', 'Negro', 'Amarillentos');
 
-/*Listo ahora seguimos con el segundo punto el punto dos*/
+/*Listo ahora seguimos con el segundo punto el punto dos
+Le inserto al Apodo la opcion no tiene*/
+
+UPDATE cat_cafe
+SET Apodo = 'no tiene'
+WHERE Apodo = '';
+
+/*Ahora el punto 3 volver el valor 'no tiene' NULL*/
+
+UPDATE cat_cafe
+SET Apodo = NULL
+WHERE Apodo= 'no tiene';
+
+
+/*Esto para el punto 4*/
+
+UPDATE cat_cafe
+SET Llego_con_alguien = 'no tiene'
+WHERE Llego_con_alguien = '';
+
+/*Esto para el punto 5*/
+
+UPDATE cat_cafe
+SET Llego_con_alguien = NULL
+WHERE Llego_con_alguien = 'ni tiene';
 
